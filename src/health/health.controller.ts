3f1123d8cacb32d85e56d0a3ed27bbfd301fe189
ghun131 <hungdh131@gmail.com>
2024-04-06 14:48:43 +0700

    feat: check health for db

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -4,19 +4,36 @@ import {
   HealthCheckResult,
   HealthCheck,
   HealthIndicatorResult,
+  TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  constructor(private health: HealthCheckService) {}
+  constructor(
+    private health: HealthCheckService,
+    private readonly db: TypeOrmHealthIndicator,
+  ) {}
+  @Get()
+  @HealthCheck()
+  check(): Promise<HealthCheckResult> {
+    return this.checkServer();
+  }
 
   @Get('server')
   @HealthCheck()
-  check(): Promise<HealthCheckResult> {
+  checkServer(): Promise<HealthCheckResult> {
     return this.health.check([
       async (): Promise<HealthIndicatorResult> => ({
         'nest-server': { status: 'up' },
       }),
+    ]);
+  }
+
+  @Get('db')
+  @HealthCheck()
+  checkDb(): Promise<HealthCheckResult> {
+    return this.health.check([
+      async (): Promise<HealthIndicatorResult> => this.db.pingCheck('postgres'),
     ]);
   }
 }
